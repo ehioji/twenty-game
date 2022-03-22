9059230cb3cf4830072188cce6dc007fc3c40dcb
Ojiefoh <ehiojiefoh@FVFZMY2RJ1WK.tld>
2022-03-22 08:27:18 -0500

    changed variable declaration

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-let userName;
-
 function askName () {
-    userName = prompt("Howdy, and welcome to Twenty! It's blackjack, but to twenty instead of twenty-one! Exciting right!? Now before we begin, I gotta know your name. Enter it below.")
+    let userName = prompt("Howdy, and welcome to Twenty! It's blackjack, but to twenty instead of twenty-one! Exciting right!? Now before we begin, I gotta know your name. Enter it below.")
     
     if (userName === "") {
         do {
